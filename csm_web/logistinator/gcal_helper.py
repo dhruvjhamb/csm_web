361b45acd8eb94278dab1a5d260d651c78564ef9
Dhruv Jhamb <dhruvsaijhamb@gmail.com>
2019-05-05 15:37:00 -0700

    Ran black

--- csm_web/logistinator/gcal_helper.py
+++ csm_web/logistinator/gcal_helper.py
@@ -1,149 +1,158 @@
 class GcalHelper:
+    def __init__(self, num_weeks):
+        self.num_weeks = num_weeks
+        self.scopes = "https://www.googleapis.com/auth/calendar"
+        self.timezone = "America/Los_Angeles"
+        self.calendar_id = "primary"
+        self.service = gcal_api_authenticate()
 
-	def __init__(self, num_weeks):
-		self.num_weeks = num_weeks
-		self.scopes = "https://www.googleapis.com/auth/calendar"
-		self.timezone = "America/Los_Angeles"
-		self.calendar_id = "primary"
-		self.service = gcal_api_authenticate()
+    def gcal_api_authenticate():
+        """Google Calendar Authentication. Returns the service object to be used
+        for making calls to the api."""
+        # token.json stores the user's access and refresh tokens. It is created
+        # automatically when the authorization flow completes for the first time.
 
-	def gcal_api_authenticate():
-	    """Google Calendar Authentication. Returns the service object to be used
-	    for making calls to the api."""
-	    # token.json stores the user's access and refresh tokens. It is created
-	    # automatically when the authorization flow completes for the first time.
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open("token.pickle", "wb") as token:
+                pickle.dump(creds, token)
 
-		creds = None
-	    # The file token.pickle stores the user's access and refresh tokens, and is
-	    # created automatically when the authorization flow completes for the first
-	    # time.
-	    if os.path.exists("token.pickle"):
-	        with open("token.pickle", "rb") as token:
-	            creds = pickle.load(token)
-	    # If there are no (valid) credentials available, let the user log in.
-	    if not creds or not creds.valid:
-	        if creds and creds.expired and creds.refresh_token:
-	            creds.refresh(Request())
-	        else:
-	            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-	            creds = flow.run_local_server()
-	        # Save the credentials for the next run
-	        with open("token.pickle", "wb") as token:
-	            pickle.dump(creds, token)
+        service = build("calendar", "v3", credentials=creds)
+        return service
 
-    	service = build("calendar", "v3", credentials=creds)
-    	return service
+    def is_room_free(event, timedelta=None):
+        """ Checks if booking event will create conflict.
+            Assumes that the first attendee of event["attendees"] is room email.
+        """
+        if not timedelta:
+            timedelta = datetime.timedelta(days=0)
 
+        start = event["start"]["dateTime"]
+        end = event["end"]["dateTime"]
+        # The first element is room email
+        room_email = event["attendees"][0]
 
-	def is_room_free(event, timedelta=None):
-		""" Checks if booking event will create conflict.
-			Assumes that the first attendee of event["attendees"] is room email.
-		"""
-		if not timedelta:
-			timedelta = datetime.timedelta(days=0)
+        timeMin = start.isoformat()
+        timeMax = end.isoformat()
 
-	    start = event["start"]["dateTime"]
-	    end = event["end"]["dateTime"]
-	    # The first element is room email
-	    room_email = event["attendees"][0]
+        # Start and End are already time zone aware
+        # However, a timezone must be provided to ensure that the
+        # result is directly translated too.
+        timeZone = TIMEZONE
+        items = [{"id": room_email}]
 
-	    timeMin = start.isoformat()
-	    timeMax = end.isoformat()
+        freebusy_query = {
+            "timeMin": timeMin + timedelta,
+            "timeMax": timeMax + timedelta,
+            "timeZone": timeZone,
+            "items": items,
+        }
 
-	    # Start and End are already time zone aware
-	    # However, a timezone must be provided to ensure that the
-	    # result is directly translated too.
-	    timeZone = TIMEZONE
-	    items = [{"id": room_email}]
+        result = self.service.freebusy().query(body=freebusy_query).execute()
+        booked_events += result["calendars"][room_email]["busy"]
+        return len(booked_events) == 0
 
-	    freebusy_query = {
-	        "timeMin": timeMin + timedelta,
-	        "timeMax": timeMax + timedelta,
-	        "timeZone": timeZone,
-	        "items": items,
-	    }
+    def is_room_free_weekly(event):
+        """ Checks if booking a recurring event will create conflict.
+            Ensure that the first attendee in event["attendees"] is the room email.
+        """
+        results = []
+        for i in range(self.num_weeks):
+            results.append(is_room_free(event, datetime.timedelta(days=7 * i)))
 
-	    result = self.service.freebusy().query(body=freebusy_query).execute()
-	    booked_events += result["calendars"][room_email]["busy"]
-	    return len(booked_events) == 0
+        return all(results)
 
+    def book_event(event):
+        """Takes in an event object as per the specification of Google Calendar's API,
+        and books it.
 
-	def is_room_free_weekly(event):
-		""" Checks if booking a recurring event will create conflict.
-			Ensure that the first attendee in event["attendees"] is the room email.
-		"""
+        deal with weekly conflicts
+        timezone issues - make everything non-time until actually booking
+        """
+        storage_data = []
+        conflicts = []
 
-		return all([is_room_free(event, datetime.timedelta(days=7*i) for i in range(self.num_weeks))])
+        if is_room_free_weekly(event) == 0 and event["weekly"]:
+            rule = f"RRULE:FREQ=WEEKLY;COUNT={self.num_weeks}"
+            event["recurrence"] = [rule]
 
+            event = (
+                self.service.events()
+                .insert(calendarId=self.calendar_id, body=event)
+                .execute()
+            )
+            storage_data.append(
+                {"Timestamp": datetime.datetime.now(), "EventID": event["id"]}
+            )
 
-	def book_event(event):
-	    """Takes in an event object as per the specification of Google Calendar's API,
-	    and books it.
+        elif event["weekly"]:
 
-	    deal with weekly conflicts
-	    timezone issues - make everything non-time until actually booking
-	    """
-	    storage_data = []
-	    conflicts = []
+            i = 0
 
-	    if is_room_free_weekly(event) == 0 and event["weekly"]:
-	        rule = f"RRULE:FREQ=WEEKLY;COUNT={self.num_weeks}"
-	        event["recurrence"] = [rule]
+            while i < self.num_weeks:
+                d = datetime.timedelta(days=7 * i)
 
-	        event = self.service.events().insert(calendarId=self.calendar_id, body=event).execute()
-	        storage_data.append({"Timestamp": datetime.datetime.now(), "EventID": event["id"]})
-	        
-	    elif event["weekly"]:
+                if not is_room_free(event, d):
 
-	        i = 0
+                    user_id = event["attendees"][1]
+                    room_id = event["attendees"][0]
+                    start_datetime = event["start"]["dateTime"]
+                    end_datetime = event["end"]["dateTime"]
+                    data = {
+                        "user_id": user_id,
+                        "room_id": room_id,
+                        "start_datetime": start_datetime,
+                        "end_datetime": end_datetime,
+                    }
+                    conflicts.append(data)
+                else:
+                    event = (
+                        self.service.events()
+                        .insert(calendarId=self.calendar_id, body=event)
+                        .execute()
+                    )
+                    storage_data.append(
+                        {"Timestamp": datetime.datetime.now(), "EventID": event["id"]}
+                    )
+                i += 1
 
-	        while i < self.num_weeks:
-	            d = datetime.timedelta(days=7 * i)
+        elif not is_room_free(event):
 
-	            if not is_room_free(event, d):
-
-	                user_id = event["attendees"][1]
-	                room_id = event["attendees"][0]
-	                start_datetime = event["start"]["dateTime"]
-	                end_datetime = event["end"]["dateTime"]
-	                data={
-	                        "user_id": user_id,
-	                        "room_id": room_id,
-	                        "start_datetime": start_datetime,
-	                        "end_datetime": end_datetime,
-	                    }
-	                conflicts.append(data)
-	            else:
-	                event = (
-	                    self.service.events()
-	                    .insert(calendarId=self.calendar_id, body=event)
-	                    .execute()
-	                )
-	                storage_data.append({
-	                    "Timestamp": datetime.datetime.now(),
-	                    "EventID": event["id"],
-	                })
-	            i += 1
-
-	        
-	    elif not is_room_free(event):
-
-        
-        	user_id = event["attendees"][1]
+            user_id = event["attendees"][1]
             room_id = event["attendees"][0]
             start_datetime = event["start"]["dateTime"]
             end_datetime = event["end"]["dateTime"]
-            data={
-                    "user_id": user_id,
-                    "room_id": room_id,
-                    "start_datetime": start_datetime,
-                    "end_datetime": end_datetime,
-                }
+            data = {
+                "user_id": user_id,
+                "room_id": room_id,
+                "start_datetime": start_datetime,
+                "end_datetime": end_datetime,
+            }
             conflicts.append(data)
-	        	
+
         else:
-	        event = self.service.events().insert(calendarId=self.calendar_id, body=event).execute()
-	        storage_data.append({"Timestamp": datetime.datetime.now(), "EventID": event["id"]})
-		        
+            event = (
+                self.service.events()
+                .insert(calendarId=self.calendar_id, body=event)
+                .execute()
+            )
+            storage_data.append(
+                {"Timestamp": datetime.datetime.now(), "EventID": event["id"]}
+            )
 
         return storage_data, conflicts
--- csm_web/logistinator/room_booker.py
+++ csm_web/logistinator/room_booker.py
@@ -11,8 +11,8 @@ import csv
 from ..scheduler.models import User
 from .models import Room
 
-class RoomBooker:
 
+class RoomBooker:
     def __init__(self):
         if len(sys.argv) > 2:
             num_weeks = int(sys.argv[1])
@@ -28,19 +28,20 @@ class RoomBooker:
 
         # Book the events
         for event in events:
-            booked, conflicts += self.gcal_helper.book_event(event)
+            booked, conflicts = self.gcal_helper.book_event(event)
 
         # Write booked events to csv
         with open("booked_events.csv", "wb") as csv_file:
-                fieldnames = ['Timestamp', 'EventID']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                for booked_event in booked:
-                    writer.writerow(booked_event)
+            fieldnames = ["Timestamp", "EventID"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            for booked_event in booked:
+                writer.writerow(booked_event)
 
         # Post conflicts
         for conflict in conflicts:
-            requests.post("http://127.0.0.1:8000/logistinator/conflict/create", data=conflict)
-
+            requests.post(
+                "http://127.0.0.1:8000/logistinator/conflict/create", data=conflict
+            )
 
     def get_matchings():
         """Call the endpoint to get the matchings, where each matching is a dictionary.
@@ -50,12 +51,11 @@ class RoomBooker:
         r = requests.get(get_matchings_url)
         return r.content
 
-
     def create_event(matching):
         """Creates an 'event' dictionary object as required by Google Calendar's
         API, and returns it.
         """
-        title = "CSM Meeting"   ### TODO: Make Matching have a title
+        title = "CSM Meeting"  ### TODO: Make Matching have a title
         user_id = matching["user_id"]
         room_id = matching["room_id"]
         start = matching["start_datetime"].isoformat()
@@ -65,7 +65,9 @@ class RoomBooker:
 
         invitees = []
 
-        user = User.get_object_or_404(pk=user_id)   ### TODO: Think about InterviewMatcher
+        user = User.get_object_or_404(
+            pk=user_id
+        )  ### TODO: Think about InterviewMatcher
         room = Room.get_object_or_404(pk=room_id)
 
         # get emails and append
